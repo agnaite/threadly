56a75978aba6e38de0cd2af7e908db97cba8bef9
Agnė Klimaitė <agnaite@me.com>
2016-03-18 13:28:07 -0700

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.new
+    @all_posts = Post.order(created_at: :desc).all
+  end
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to '/'
+      end
+  end
+  private
+    def post_params
+      params.require(:post).permit(:comment)
+    end
+end
