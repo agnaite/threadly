fe44c4ddffa755c45da496b1a188bac43b81337a
Agnė Klimaitė <agnaite@me.com>
2016-03-18 18:26:38 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20160317233715) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
