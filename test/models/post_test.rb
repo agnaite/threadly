61b7511caa4bb577987f4fda1e0afeda5f3a7c1b
Agnė Klimaitė <agnaite@me.com>
2016-03-30 18:30:00 -0700

    wrote some tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it can be created' do
+    assert Post.new(comment: 'this is a test').save == true
+  end
+
+  test 'must have a comment to be valid' do
+    assert Post.new(comment: nil).valid? == false
+  end
+
+  test 'has a comment' do
+    post = Post.new(comment: 'this is a test')
+    post.save!
+    assert post.reload.comment.nil? == false
+  end
+
+  test 'it can be assigned to a user' do
+    user = User.create! password: 'atestpassword', first_name: 'Hector', last_name: 'Cat', email: 'hector@pivotbio.com'
+    post = Post.new(comment: 'test', user: user)
+    post.save!
+    assert post.reload.user == user
+  end
 end
